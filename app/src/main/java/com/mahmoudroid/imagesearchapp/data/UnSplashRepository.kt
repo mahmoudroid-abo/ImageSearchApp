041852a635276ae1c833e8cab328afef023541ba
maboe <maboelenen55@gmail.com>
2021-02-03 22:58:16 +0200

    add PagingSource & PagingData

--- app/src/main/java/com/mahmoudroid/imagesearchapp/data/UnSplashRepository.kt
+++ app/src/main/java/com/mahmoudroid/imagesearchapp/data/UnSplashRepository.kt
@@ -1,10 +1,22 @@
 package com.mahmoudroid.imagesearchapp.data
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
 import com.mahmoudroid.imagesearchapp.api.UnSplashApi
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class UnSplashRepository @Inject constructor(private val UnSplashApi: UnSplashApi) {
+class UnSplashRepository @Inject constructor(private val unsplashApi: UnSplashApi) {
 
+    fun getSearchResults(query: String) =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                maxSize = 100,
+                enablePlaceholders = false,
+            ),
+            pagingSourceFactory = { UnSplashPagingSource(unsplashApi, query) }
+        ).liveData
 }
--- app/src/main/java/com/mahmoudroid/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/mahmoudroid/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -1,9 +1,22 @@
 package com.mahmoudroid.imagesearchapp.ui.gallery
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.mahmoudroid.imagesearchapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+
+    private val viewModel by viewModels<GalleryViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.photos.observe(viewLifecycleOwner) {
+
+        }
+    }
 }
