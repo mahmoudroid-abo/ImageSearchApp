f0826ad2bee0f21c855ca9fe66f623321ce84984
maboe <maboelenen55@gmail.com>
2021-02-06 02:22:48 +0200

    add search

--- app/src/main/java/com/mahmoudroid/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/mahmoudroid/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -1,7 +1,10 @@
 package com.mahmoudroid.imagesearchapp.ui.gallery
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.mahmoudroid.imagesearchapp.R
@@ -27,14 +30,40 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
         binding.apply {
             recyclerView.setHasFixedSize(true)
             recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
-                header = UnSplashPhotoLoadStateAdapter{adapter.retry()},
-                footer = UnSplashPhotoLoadStateAdapter{adapter.retry()},
+                header = UnSplashPhotoLoadStateAdapter { adapter.retry() },
+                footer = UnSplashPhotoLoadStateAdapter { adapter.retry() },
             )
         }
 
         viewModel.photos.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.menu_gallery, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) {
+                    binding.recyclerView.scrollToPosition(0)
+                    viewModel.searchPhotos(query)
+                    searchView.clearFocus()
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
     }
 
     override fun onDestroyView() {
